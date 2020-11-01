3e8da7ae6eaddfc70de150c4b2ddd350e0fd4f3c
jsingle345 <jsingle345@gmail.com>
2020-11-01 10:32:00 -0600

    commit tree d523c6587d42939a1574a243496e82421d0f2816

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfPeople, party){
+    const message = []
+
+    for(let i = 0; i < arrayOfPeople.length; i++){
+        message.push(`Thank you, ${arrayOfPeople[i]}, for the wonderful ${party} gift!`)
+    }
+    return message
+}
+
+
+countDown = (num) => {
+    while(num >= 0) {
+        console.log(num)
+    num--
+    }
+}
